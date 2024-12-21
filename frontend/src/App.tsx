9a611525faad9a4ae828b37bb0d681fadca71e7a
rohan <gargrohan138@gmail.com>
2024-12-21 16:55:23 +0530

    created signin page and route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.css'
 import Signup from './pages/signup'
+import Signin from './pages/signin'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/signup' element={<Signup />} />
+        <Route path='/signin' element={<Signin />} />
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/pages/signin/index.tsx
+++ frontend/src/pages/signin/index.tsx
@@ -5,20 +5,19 @@ import { NormalInut, PasswordInput } from "@/components/inputs/normal";
 import { SignUpInButton } from "@/components/buttons/signupsignin";
 import { SignUpInBottom } from "@/components/subheadings/bottom";
 
-export default function Signup(){
+export default function Signin(){
     const [isvisible, setIsVisible] = useState(false);
 
     return <div className="w-screen h-screen bg-gray-300">
         <div className="w-full h-full flex justify-center">
             <div className="h-full flex flex-col justify-center max-lg:w-full max-xl:mx-8 lg:w-4/12">
                 <div className="w-full bg-white h-max flex flex-col p-5 rounded-lg shadow-sm">
-                    <SignUpInHeading title="Sign up" />
-                    <SignUpInSubHeading title="Enter your information to create an account" />
-                    <NormalInut label="Name" placeholder="Prem Chand" type="text" id="name" />
+                    <SignUpInHeading title="Sign in" />
+                    <SignUpInSubHeading title="Enter your credentials to access your account" />
                     <NormalInut label="Email" type="email" placeholder="premchand@gmail.com" id="email" />
                     <PasswordInput label="Password" id="password" isVisible={isvisible} setIsVisible={setIsVisible} />
-                    <SignUpInButton title="Sign up with Email" />
-                    <SignUpInBottom title="Already have an account? " underlined="Sign in" />
+                    <SignUpInButton title="Sign in with Email" />
+                    <SignUpInBottom title="Don't have an account? " underlined="Sign up" />
                 </div>
             </div>
         </div>
